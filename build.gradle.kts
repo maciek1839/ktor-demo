938d34bab2f98d8eac7c5bb6d7c3e208a7daa822
Maciek <maciek1839@gmail.com>
2023-06-17 16:11:59 +1000

    chore: set up SonarCloud integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,14 @@ plugins {
     kotlin("jvm") version "1.6.10"
     id("io.ktor.plugin") version "2.2.3"
 
+    id("org.sonarqube") version "4.2.1.3168"
+
     id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
 }
 
 group = "com.showmeyourcode.ktor.demo"
 version = "0.0.1"
+
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 
@@ -20,6 +23,13 @@ application {
 
 repositories {
     mavenCentral()
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "ShowMeYourCodeYouTube_ktor-demo")
+        property("sonar.organization", "showmeyourcodeyoutube")
+    }
 }
 
 dependencies {
